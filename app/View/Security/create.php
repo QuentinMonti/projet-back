6d91708c775863c98a27fdb7007879f75601e280
quentin monteferrario <quentin.monteferrario@fcinq.com>
2022-03-09 16:13:34 +0100

    verif login

---
+++ app/View/Security/create.php
@@ -0,0 +1,14 @@
+<h1>Please create acompte</h1>
+
+<form method="post">
+    <div class="mb-3">
+        <label for="exampleInputUsername" class="form-label">Username</label>
+        <input type="text" class="form-control" id="exampleInputUsername" aria-describedby="usernameHelp" name="username">
+        <div id="usernameHelp" class="form-text">We'll never share your username with anyone else.</div>
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
--- app/src/Controller/SecurityController.php
+++ app/src/Controller/SecurityController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Controller;
 
+namespace App\Controller;
+use PDO;
 use Cacofony\BaseClasse\BaseController;
 use Cacofony\Helper\AuthHelper;
 use App\Manager\UserManager;
@@ -26,17 +27,24 @@ class SecurityController extends BaseController
     public function postLogin(UserManager $userManager)
     {
         $users = $userManager->findAll();
+        $connect = false;
 
         foreach ($users as $user)
         {
             if($user->username == $_POST['username'] && $user->pwd == $_POST['password'])
             {
-                $jwt = JWT::encode(['user' => $this->HTTPRequest->getRequest('username')], $_ENV['APP_SECRET']);
-                $_SESSION['user_badge'] = $jwt;
-                $this->HTTPResponse->redirect('/');
+                $connect = true;
             }
         }
 
+        if($connect)
+        {
+            $jwt = JWT::encode(['user' => $this->HTTPRequest->getRequest('username')], $_ENV['APP_SECRET']);
+            $_SESSION['user_badge'] = $jwt;
+            $this->HTTPResponse->redirect('/');
+        } else { 
+            echo("usename or password incorrect");
+        }
     }
 
     /**
@@ -47,5 +55,46 @@ class SecurityController extends BaseController
     {
         AuthHelper::logout();
         $this->HTTPResponse->redirect('/');
+    }
+
+    /**
+     * @Route(path="/create")
+     * @return void
+     */
+    public function getCreate()
+    {
+        $this->render('Security/create', [], '');
+    }
+
+     /**
+     * @Route(path="/login")
+     * @param UserManager $userManager
+     * @return void
+     */
+    public function postCreate(UserManager $userManager)
+    {
+        if(isset($_POST['username']) && isset($_POST['password'])  ){
+            $servername = "localhost";
+            $username = "username";
+            $password = "password";
+            $dbname = "mt4-data";
+            $name = $_POST['username'];
+            $pwd = $_POST['password'];
+
+            try {
+                $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                // set the PDO error mode to exception
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $sql = "INSERT INTO User (username, pwd)
+                VALUES ($name, $pwd)";
+                // use exec() because no results are returned
+                $conn->exec($sql);
+                echo "New record created successfully";
+                } catch(PDOException $e) {
+                echo $sql . "<br>" . $e->getMessage();
+            }
+
+            $conn = null;
+        }
     }
 }
